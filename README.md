c4c9dc491620c4f54b89e32b060fef47a1a5fa6e
hassan <hasan@digilite.ca>
2022-10-09 20:29:09 +0330

    First Push

---
+++ README.md
@@ -0,0 +1,8 @@
+# eterni
+
+Eterni VPN
+
+## Getting Started
+
+This project is a starting point for a VPN application.
+ 
---
+++ android/app/src/main/kotlin/com/example/eterni/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eterni
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
